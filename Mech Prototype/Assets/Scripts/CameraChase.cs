5a0fc99b293b92e030601d41c94bf931671b0d41
gmd1apeterbos <97040824@st.deltion.nl>
2018-01-15 16:40:04 +0100

    Project and basic controls

---
+++ Mech Prototype/Assets/Scripts/CameraChase.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraChase : MonoBehaviour {
+    public List<Transform> targets = new List<Transform>();
+    public Vector3 offset;
+    public float smoothTime;
+
+    public float minZoom;
+    public float maxZoom;
+    public float zoomLimit;
+
+    private Vector3 velocity;
+    private Camera myCam;
+
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+    }
+
+	void LateUpdate () {
+        if(targets.Count > 0)
+        {
+            Move();
+            Zoom();
+        }
+      
+
+	}
+
+    void Move()
+    {
+        Vector3 center = GetCenter();
+
+        Vector3 newPos = center + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
+    }
+
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimit);
+        myCam.fieldOfView = Mathf.Lerp(myCam.fieldOfView, newZoom, Time.deltaTime);
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenter()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center;
+    }
+}
---
+++ Mech Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [Header("Basic Variables")]
+    public int playerNumber;
+    public float moveSpeed;
+    public float rotateSpeed;
+    public float jumpForce;
+
+    public bool usingController;
+    private Rigidbody rb;
+
+    [Header("Modular Parts")]
+    public GameObject myBody;
+    public GameObject armsParent;
+    public GameObject myLeftArm;
+    public GameObject myRightArm;
+    public GameObject myLegs;
+    Vector3 direction;
+    Camera myCam;
+    // Use this for initialization
+    void Start () {
+            rb = GetComponent<Rigidbody>();
+        myCam = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate () {
+
+        Move();
+        Jump();
+    }
+
+    public void Move()
+    {
+        if (playerNumber == 1)
+        {
+            if (usingController)
+            {
+                float z = Input.GetAxis("LeftJoyVertical");
+                float x = Input.GetAxis("LeftJoyHorizontal");
+                transform.position += transform.forward * -z * moveSpeed * Time.deltaTime ;
+                transform.Rotate(transform.rotation.x, x * rotateSpeed * Time.deltaTime, transform.rotation.z);
+
+                float rotY = Input.GetAxis("RightJoyHorizontal");
+           
+
+                myBody.transform.Rotate(transform.rotation.x, rotY * rotateSpeed * Time.deltaTime, transform.rotation.z);
+            }
+            else
+            {
+                float z = Input.GetAxis("Vertical");
+                float x = Input.GetAxis("Horizontal");
+                transform.position += transform.forward * z * moveSpeed * Time.deltaTime;
+                transform.Rotate(transform.rotation.x, x * rotateSpeed * Time.deltaTime, transform.rotation.z);
+
+                float rotY = Input.GetAxis("QE");
+
+
+                myBody.transform.Rotate(transform.rotation.x, rotY * rotateSpeed * Time.deltaTime, transform.rotation.z);
+            }
+
+           
+        }
+        else if (playerNumber == 2)
+        {
+            if (usingController)
+            {
+                float z = Input.GetAxis("LeftJoyVerticalPtwo");
+                float x = Input.GetAxis("LeftJoyHorizontalPtwo");
+
+                transform.position += transform.forward * -z * moveSpeed * Time.deltaTime;
+                transform.Rotate(transform.rotation.x, x * rotateSpeed * Time.deltaTime, transform.rotation.z);
+
+                float rotY = Input.GetAxis("RightJoyHorizontalPtwo");
+
+
+                myBody.transform.Rotate(transform.rotation.x, rotY * rotateSpeed * Time.deltaTime, transform.rotation.z);
+            }
+            else
+            {
+                float z = Input.GetAxis("VerticalArrows");
+                float x = Input.GetAxis("HorizontalArrows");
+
+                transform.position += transform.forward * z * moveSpeed * Time.deltaTime;
+                transform.Rotate(transform.rotation.x, x * rotateSpeed * Time.deltaTime, transform.rotation.z);
+
+                float rotY = Input.GetAxis("79");
+
+
+                myBody.transform.Rotate(transform.rotation.x, rotY * rotateSpeed * Time.deltaTime, transform.rotation.z);
+            }
+          
+
+        }
+    }
+
+    public void Jump()
+    {
+        if(playerNumber == 1)
+        {
+            if (Input.GetButtonDown("Abutton") || Input.GetButtonDown("Space"))
+            {
+                this.rb.AddForce(Vector3.up * jumpForce * 2 * Time.deltaTime);
+            }
+        }
+        else if (playerNumber == 2)
+        {
+            if (Input.GetButtonDown("AbuttonPtwo") || Input.GetButtonDown("NumEnter"))
+            {
+                this.rb.AddForce(Vector3.up * jumpForce * 2 * Time.deltaTime);
+            }
+        }
+    }
+
+   
+}
