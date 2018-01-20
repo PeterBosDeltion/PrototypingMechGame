5336c15f27bf5e15d1b1d5d2c0bfc45627a93169
gmd1apeterbos <97040824@st.deltion.nl>
2018-01-20 17:23:48 +0100

    Added flamethrower

---
+++ Mech Prototype/Assets/Scripts/Weapons/Flame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Player" && other.GetComponent<Player>().playerNumber != GetComponentInParent<Player>().playerNumber)
+        {
+            Debug.Log("Hit opponent");
+            Player hitPlayer = other.GetComponent<Player>();
+            hitPlayer.hp -= GetComponentInParent<Flamethrower>().damage;
+        }
+    }
+}
---
+++ Mech Prototype/Assets/Scripts/Weapons/Flamethrower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : Weapon {
+    public ParticleSystem parts;
+    // Use this for initialization
+    void Start () {
+        parts = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Use(Vector3 armPos)
+    {
+        if (!onCooldown)
+        {
+            parts.Play();
+            StartCoroutine(Cooldown());
+        }
+    }
+
+    public override IEnumerator Cooldown()
+    {
+        parts.Stop();
+        onCooldown = true;
+        yield return new WaitForSeconds(cooldown);
+        onCooldown = false;
+    }
+
+}
